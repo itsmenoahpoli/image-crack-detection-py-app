cbd3379d38d510c91b80576802bad2d1fd1cfb8f
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2022-05-25 03:42:11 -0700

    Added image file upload (for temporary image to be processed), added the library method to api endpoint (/image/process)

--- server-api/lib/crack_detection_lib.py
+++ server-api/lib/crack_detection_lib.py
@@ -1,17 +1,10 @@
-from fastapi import FastAPI, File, UploadFile
-from pydantic import BaseModel
-
-
-
-# Image crack detection library
 import cv2
 import math
 import numpy as np
 import scipy.ndimage
 
-app = FastAPI()
-
-def process_uploaded_image(imageDir):
+class CrackDetectionLib:
+  def process_uploaded_image(imageDir):
 
     # return imageDir
 
@@ -39,7 +32,7 @@ def process_uploaded_image(imageDir):
         return data_max
 
     # start calulcation
-    gray_image = cv2.imread(r'storage/crack1.jpeg', 0) ### CRACK IMAGE TO BE PROCESSED (VALUE WILL BE IMAGE PATH)
+    gray_image = cv2.imread(r'/', 0) ### CRACK IMAGE TO BE PROCESSED (VALUE WILL BE IMAGE PATH)
 
     with_nmsup = True #apply non-maximal suppression
     fudgefactor = 1.3 #with this threshold you can play a little bit
@@ -65,10 +58,8 @@ def process_uploaded_image(imageDir):
         mag = cv2.normalize(mag, 0, 255, cv2.NORM_MINMAX)
         kernel = np.ones((5,5),np.uint8)
         result = cv2.morphologyEx(mag, cv2.MORPH_CLOSE, kernel)
-        # cv2.imshow('im', result)
-        # cv2.waitKey()
-
-        return result
+        cv2.imshow('im', result)
+        cv2.waitKey()
 
     #or apply a non-maximal suppression
     else:
@@ -82,29 +73,5 @@ def process_uploaded_image(imageDir):
         kernel = np.ones((5,5),np.uint8)
         result = cv2.morphologyEx(mag, cv2.MORPH_CLOSE, kernel)
 
-        # cv2.imshow('im', result)
-        # cv2.waitKey()
-
-        return result
-
-# ---------------------------------------------------------------------------------------------------------- #
-
-class ImageModel(BaseModel):
-  filetype: str
-
-@app.post('/api/v1/image/process')
-async def process_image(file: UploadFile):
-  try:
-    temp_uploaded_file_location = f"storage/{file.filename}"
-    with open(temp_uploaded_file_location, "wb+") as file_object:
-        file_object.write(file.file.read())
-
-    return process_uploaded_image(temp_uploaded_file_location)
-
-    # return {
-    #   "message": f"file '{file.filename}' saved at '{temp_uploaded_file_location}'"
-    # }
-  except Exception as error:
-    return {
-      "error": repr(error)
-    }
+        cv2.imshow('im', result)
+        cv2.waitKey()
